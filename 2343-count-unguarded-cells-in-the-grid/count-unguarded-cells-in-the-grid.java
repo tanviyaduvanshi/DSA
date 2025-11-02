232443d406229f5d18392813278dbc39fac88665
Tanvi yaduvanshi <tanvithakur1215@gmail.com>
2025-11-02 22:32:33 +0530

    Time: 16 ms (100.00%) | Memory: 146.7 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -5,28 +5,27 @@ class Solution {
             grid[g[0]][g[1]]=1;
         }
         for(int []w:walls){
-            grid[w[0]][w[1]]=2;
+            grid[w[0]][w[1]]=1;
         }
         for (int[] g : guards) {
         int r = g[0], c = g[1];
-    // do whatever you need with this guard
             if(r>0){
-                for(int i=r-1;i>=0&&grid[i][c]!=1&&grid[i][c]!=2;i--){
+                for(int i=r-1;i>=0&&grid[i][c]!=1;i--){
                     grid[i][c]=3;
                 }
             }
             if(r<m){
-                for(int i=r+1;i<m&&grid[i][c]!=1&&grid[i][c]!=2;i++){
+                for(int i=r+1;i<m&&grid[i][c]!=1;i++){
                    grid[i][c]=3;
                 }
             }
             if(c>0){
-                for(int i=c-1;i>=0&&grid[r][i]!=1&&grid[r][i]!=2;i--){
+                for(int i=c-1;i>=0&&grid[r][i]!=1;i--){
                     grid[r][i]=3;
                 }
             }
             if(c<n){
-                for(int i=c+1;i<n&&grid[r][i]!=1&&grid[r][i]!=2;i++){
+                for(int i=c+1;i<n&&grid[r][i]!=1;i++){
                     grid[r][i]=3;
                 }
             }
