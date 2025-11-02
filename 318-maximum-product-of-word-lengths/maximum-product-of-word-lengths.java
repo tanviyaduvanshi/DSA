8bef14a8298a739c9969592604f9c95a3a32537f
Tanvi yaduvanshi <tanvithakur1215@gmail.com>
2025-11-02 23:16:44 +0530

    Time: 11 ms (43.86%) | Memory: 48.7 MB (11.05%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int[]value=new int [words.length];
+        for(int i=0;i<words.length;i++){
+            String temp=words[i];
+            value[i]=0;
+            for(int j=0;j<temp.length();j++){
+                value[i]|=(1<<temp.charAt(j)-'a');
+            }
+        }int maxProduct = 0;
+        for(int i=0;i<words.length;i++){
+        for(int j=i+1;j<words.length;j++){
+            if((value[i]&value[j])==0){
+                maxProduct=Math.max(maxProduct,words[i].length()*words[j].length());
+            }
+        }
+    }
+    
+    return maxProduct;
+    }
+}
