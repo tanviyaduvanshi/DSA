39d439bc4870c11448cae99401cd1e38e84e8d94
Tanvi yaduvanshi <tanvithakur1215@gmail.com>
2025-11-08 16:34:47 +0530

    Time: 1 ms (10.00%) | Memory: 42.6 MB (5.00%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        if(n==0)return 0;
+        int[]f=new int[31];
+        f[0]=1;
+        for(int i=1;i<31;i++){
+            f[i]=2*f[i-1]+1;
+        }
+        int result=0;
+        int sign=1;
+        for(int i=30;i>=0;i--){
+            int ith=((1<<i)&n);
+            if(ith==0)continue;
+            if(sign>0){
+                result+=f[i];
+            }
+            else{
+                result-=f[i];
+            }
+            sign=sign*(-1);
+        }return result;
+    }
+}
